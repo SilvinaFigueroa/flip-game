516e1b69dd06b2fb9decdcc26af9f9d4aea75226
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-05-23 18:26:36 -0700

    event listener for card clicked

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,21 +1,32 @@
 
 let form = document.getElementById("userForm")
-let container = document.getElementById("container")
-let gridSize = 8;
+let formContainer = document.getElementById("form-container")
+let gridSize = 3;
+
+// Create container for the game grid
+let gridContainer = document.createElement("div")
+gridContainer.classList.add('grid-container')
+
 
 console.log(gridSize)
 
-form.addEventListener('submit', function(event) {
+// When form submited, initialize the game
+form.addEventListener('submit', (event) => {
     event.preventDefault()
     playGame()
 })
 
+// create table
+let table = document.createElement("table")
+console.log(`Table created`)
+
 function playGame() {
-    let table = document.createElement("table")
-    console.log(`Table created`)
+
     for (let i = 0; i < gridSize; i++) {
+        // create each row
         const row = document.createElement("tr")
         console.log(`row ${i} created`)
+        // create and attach cells to row
         for (let j = 0; j < gridSize; j++) {
             const cell = document.createElement("td")
             cell.classList.add("card")
@@ -23,10 +34,39 @@ function playGame() {
             console.log(`cell ${j} created`)
             row.appendChild(cell)
         }
+        // attach row with all the cells to the table
         table.appendChild(row)
     }
-
-    form.remove()
-    container.appendChild(table)
-
+    // Remove the form container to replace it with the gridContainer (game)
+    formContainer.remove()
+    gridContainer.appendChild(table)
+    document.body.append(gridContainer)
 }
+
+table.addEventListener('click', (cardClicked) => {
+    cardClicked.preventDefault()
+    console.log(cardClicked.target)
+    if (cardClicked.target instanceof HTMLTableCellElement) {
+        flipCard(cardClicked)
+    }
+})
+
+function flipCard(clickedCell) {
+    // Read indexed of table: (cell, row)
+    let cellIndex = clickedCell.target.cellIndex;
+    let rowIndex = clickedCell.target.parentNode.rowIndex;
+
+    clickedCell.target.style.backgroundColor = "green";
+    clickedCell.target.classList.add('flip-card')
+
+    console.log(cellIndex, rowIndex)
+
+    // (rowIndex, cellIndex + 1).style.backgroundColor = "green"
+
+    // Validation: The index of the row is-1 if the row is not part of a table.
+
+    // let table = clickedCell.parentNode.parentNode; // This is the table element
+    // if (table.rows[rowIndex] && table.rows[rowIndex].cells[cellIndex + 1]) {
+    //     table.rows[rowIndex].cells[cellIndex + 1].style.backgroundColor = "green";
+    }
+
